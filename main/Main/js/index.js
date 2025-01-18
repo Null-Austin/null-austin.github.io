80f850b1073ffe4051e85fcf3a0a28310831ae97
austin <nullaustin2007@gmail.com>
2025-01-18 12:35:50 -0600

    made projects dynamic with js

--- main/Main/js/index.js
+++ main/Main/js/index.js
@@ -1,3 +1,21 @@
-document.addEventListener('DOMContentLoaded', function() {
-    console.log('page fully loaded')
+document.addEventListener('DOMContentLoaded', () => {
+    const projects = {
+        'BestJS': {
+            'title': 'BestJS',
+            'Desc': 'My favorite JavaScript library, to simply make JS!',
+            'Link': 'https://github.com/Null-Austin/BestJS'
+        },
+        'Quick VSCODE': {
+            'title': 'Quick VSCODE',
+            'Desc': 'A quick way to load up VSCODE',
+            'Link': 'https://chromewebstore.google.com/detail/quick-vscode/mcppolgkeojkenmalifhoabdlpjbkfko'
+        }
+    };
+
+    Object.keys(projects).forEach(projectKey => {
+        const project = projects[projectKey];
+        let html = `<p><a href="${project.Link}">${project.title}</a> - ${project.Desc}</p>`;
+
+        document.getElementById('projects').innerHTML += html;
+    });
 });
